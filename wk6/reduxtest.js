c9a99ef8c1dc620d07a707e0510674f730c09f5c
shodan993 <paulnstanley@gmail.com>
2018-08-27 14:01:02 -0400

    init todo-redux

---
+++ wk6/reduxtest.js
@@ -0,0 +1,41 @@
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+  };
+};
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'RESET':
+      return 0;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(counter);
+
+store.dispatch({ type:'INCREMENT' });
+
+console.log(store.getState());
