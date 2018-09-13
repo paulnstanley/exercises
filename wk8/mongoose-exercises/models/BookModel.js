f5e97eb814109b181b46148cfb47a274a28ededc
shodan993 <paulnstanley@gmail.com>
2018-09-13 12:47:45 -0400

    adding mongoose-exercises

---
+++ wk8/mongoose-exercises/models/BookModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: String,
+  author: String,
+  pages: Number,
+  genres: [],
+  rating: Number,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
