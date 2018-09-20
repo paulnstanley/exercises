55231d2a2560c90333eb8b3f2b1ec2d3ba33be16
shodan993 <paulnstanley@gmail.com>
2018-09-20 11:36:54 -0400

    pt1 complete

---
+++ wk9/feedbackLoop/server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '425873972634-52hb8283csmg1o6a19dk7h8mgfq3sbkb.apps.googleusercontent.com',
+  googleClientSecret: 'jtnMG9MkAwWOLzPrDZ8aRwOf',
+  mongoURI: 'mongodb://localhost/feedback-loop',
+  cookieKey: 'somerandomthing'
+}
